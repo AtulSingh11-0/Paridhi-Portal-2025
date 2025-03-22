6967dc7dcc357a0fa6075b996f0fcbc1e260c988
Atul Singh <cocatul11@gmail.com>
2025-03-22 06:20:26 +0530

    Add application properties and test class for Paridhi Portal 2025

---
+++ src/test/java/com/megatronix/paridhi/ParidhiPortal2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megatronix.paridhi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParidhiPortal2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
