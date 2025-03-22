344cc885c16d15c3f62e7e7ef24086e765e18c14
Atul Singh <cocatul11@gmail.com>
2025-03-22 06:20:08 +0530

    Add main application class for Paridhi Portal 2025 with basic setup

---
+++ src/main/java/com/megatronix/paridhi/ParidhiPortal2025Application.java
@@ -0,0 +1,31 @@
+package com.megatronix.paridhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@EnableAsync
+@EnableWebMvc
+@RestController
+@EnableScheduling
+@SpringBootApplication
+@EnableTransactionManagement
+public class ParidhiPortal2025Application {
+	public static void main(String[] args) {
+		SpringApplication.run(ParidhiPortal2025Application.class, args);
+		log.info("Portal successfully running on http://localhost:8080");
+	}
+
+	@GetMapping
+	public String home() {
+		return "Hello";
+	}
+}
