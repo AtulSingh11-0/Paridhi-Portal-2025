cd549813a8b5bee8dbd703216debe730ea5eb677
Atul Singh <cocatul11@gmail.com>
2025-03-22 06:28:33 +0530

    Add custom exceptions for user management: UserAlreadyExistsException and UserAlreadyVerifiedException

---
+++ src/main/java/com/megatronix/paridhi/exception/UserAlreadyVerifiedException.java
@@ -0,0 +1,7 @@
+package com.megatronix.paridhi.exception;
+
+public class UserAlreadyVerifiedException extends RuntimeException {
+  public UserAlreadyVerifiedException(String message) {
+    super(message);
+  }  
+}
