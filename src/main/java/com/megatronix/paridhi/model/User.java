78898765d741532aeb75eea4ffc41dac5a8f1cc8
Atul Singh <cocatul11@gmail.com>
2025-03-23 12:24:53 +0530

    fix-7: mark gids list as transient to prevent serialization issues

--- src/main/java/com/megatronix/paridhi/model/User.java
+++ src/main/java/com/megatronix/paridhi/model/User.java
@@ -70,7 +70,7 @@ public class User implements UserDetails {
     private String rollNo;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
-    private List<MRD> gids = new ArrayList<>();
+    private transient List<MRD> gids = new ArrayList<>();
 
     @Builder.Default
     private boolean isPaid = false;
