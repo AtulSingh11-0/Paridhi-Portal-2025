b5762b39a38c3c2c0f443f622da5df1021d65a62
Atul Singh <cocatul11@gmail.com>
2025-04-04 18:15:11 +0530

    feat: add caching configuration with Caffeine for improved performance

--- src/main/java/com/megatronix/paridhi/ParidhiPortal2025Application.java
+++ src/main/java/com/megatronix/paridhi/ParidhiPortal2025Application.java
@@ -2,6 +2,7 @@ package com.megatronix.paridhi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -14,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @EnableAsync
 @EnableWebMvc
+@EnableCaching
 @RestController
 @EnableScheduling
 @SpringBootApplication
