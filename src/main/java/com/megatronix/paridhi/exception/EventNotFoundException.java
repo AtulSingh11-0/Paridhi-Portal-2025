01d74979ec2084c17ee01b228477372bce9518d6
Atul Singh <cocatul11@gmail.com>
2025-04-14 14:59:29 +0530

    feat: add serialVersionUID to EventNotFoundException for serialization compatibility

--- src/main/java/com/megatronix/paridhi/exception/EventNotFoundException.java
+++ src/main/java/com/megatronix/paridhi/exception/EventNotFoundException.java
@@ -1,7 +1,15 @@
 package com.megatronix.paridhi.exception;
 
 public class EventNotFoundException extends RuntimeException {
-  public EventNotFoundException(String message) {
+  
+	private static final long serialVersionUID = 1L;
+	
+	public EventNotFoundException(String message) {
     super(message);
   }
+  
+  public EventNotFoundException(Long eventId) {
+    super("Event not found with ID: " + eventId);
+  }
+
 }
