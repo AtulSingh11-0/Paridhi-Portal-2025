febf9e2c341ddbbcbe9ba362c3d94434ed40daac
Atul Singh <cocatul11@gmail.com>
2025-05-11 18:50:19 +0530

    refactor: improve formatting and consistency in TeamSecurity class methods

--- src/main/java/com/megatronix/paridhi/security/TeamSecurity.java
+++ src/main/java/com/megatronix/paridhi/security/TeamSecurity.java
@@ -13,20 +13,28 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class TeamSecurity {
-    private final TeamService teamService;
-    private final MRDRepository mrdRepository;
+	private final TeamService teamService;
+	private final MRDRepository mrdRepository;
 
-    public boolean isTeamMember(String tid, String email) {
-        try {
-            TeamResponse team = teamService.getTeamByTid(tid);
+	public boolean isTeamMember(String tid, String email) {
+		try {
+			TeamResponse team = teamService.getTeamByTid(tid);
 
-						//get GIDs for the email
-						List<String> gids = mrdRepository.findGidListByUserEmail(email);
+			// get GIDs for the email
+			List<String> gids = mrdRepository.findGidListByUserEmail(email);
 
-						// check if any of the user's GIDs are in the team
-						return team.getGidList().stream().anyMatch(gids::contains);
-        } catch (Exception e) {
-            return false;
-        }
-    }
+			// check if any of the user's GIDs are in the team
+			return team.getGidList().stream().anyMatch(gids::contains);
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public boolean isGidOwner(String gid, String email) {
+		try {
+			return mrdRepository.existsByGidAndUserEmail(gid, email);
+		} catch (Exception e) {
+			return false;
+		}
+	}
 }
